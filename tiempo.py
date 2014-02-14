df24e4479194e62d7a4052eb2a7b441d18433558
usuario <usuario@debian.gonzalonazareno.org>
2014-02-14 10:08:43 +0100

    añadir jinja2

--- tiempo.py
+++ tiempo.py
@@ -3,29 +3,31 @@ import json
 import os
 import requests
 from jinja2 import Template
-html = open('index.html','w')
+html = open('template.html','r')
 
 def direccion(orientacion):
-	if (orientacion > 337,5 and orientacion < 360) or (orientacion  > 0 and orientacion < 22.5):
+	if (orientacion > 337.5 and orientacion < 360) or (orientacion  > 0 and orientacion < 22.5):
 		return 'N'
-	if orientacion > 22,5 and orientacion < 67,5:
+	if orientacion > 22.5 and orientacion < 67.5:
 		return 'NE'
-	if orientacion > 67,5 and orientacion < 112,5:
+	if orientacion > 67.5 and orientacion < 112.5:
 		return 'E'
-	if orientacion > 112,5 and orientacion < 157,5:
+	if orientacion > 112.5 and orientacion < 157.5:
 		return 'SE'
-	if orientacion > 157,5 and orientacion < 202,5:
+	if orientacion > 157.5 and orientacion < 202.5:
 		return 'S'
-	if orientacion > 202,5 and orientacion < 245,5:
+	if orientacion > 202.5 and orientacion < 245.5:
 		return 'SO'
-	if orientacion > 245,5 and orientacion < 292,5:
+	if orientacion > 245.5 and orientacion < 292.5:
 		return 'O'
-	if orientacion > 292,5 and orientacion < 337,5:
+	if orientacion > 292.5 and orientacion < 337.5:
 		return 'NO'
 
 print ''
 
-provincias = {'1':'Almería','2':'Cádiz','3':'Córdoba','4':'Granada','5':'Huelva','6':'Jaén','7':'Málaga','8':'Sevilla'}
+provincias = {'1':'Almeria','2':'Cadiz','3':'Cordoba','4':'Granada','5':'Huelva','6':'Jaen','7':'Malaga','8':'Sevilla'}
+
+
 
 for numero in provincias:
 	print provincias[numero]
@@ -34,7 +36,7 @@ print ''
 
 respuesta = raw_input('Elige una Provincia para ver el tiempo: ')
 
-os.system('clear')
+#os.system('clear')
 
 print 'El tiempo para la Provincia de:',provincias[respuesta]
 
@@ -53,9 +55,13 @@ print 'La temperatura actual de %s es de %s grados centígrados' % (provincias[r
 print speed
 print orientacion
 
-html.write()
 
-provincia = Template('hello {{ name }}!')
-print template.render(name=provincias[numero])
+f = open ('ejempl1.html','r')
+html =''
 
-html.close()
+for linea in f:
+	html += linea
+template.render(name=html)
+
+mitemplate = Template(' {{ provincia }}!')
+print mitemplate.render(name=provincias[respuesta])
