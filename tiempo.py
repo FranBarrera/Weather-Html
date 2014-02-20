01935173235e343b172bbffc70e22dabd10525f4
FranBarrera6 <franbarrera6@gmail.com>
2014-02-20 17:59:45 +0100

    creacion de listas para jinja

--- tiempo.py
+++ tiempo.py
@@ -10,44 +10,50 @@ web = open('web.html','w')
 def direccion(orientacion):
 	if (orientacion > 337.5 and orientacion < 360) or (orientacion  > 0 and orientacion < 22.5):
 		return 'N'
-	if orientacion > 22.5 and orientacion < 67.5:
+	if orientacion >= 22.5 and orientacion <= 67.5:
 		return 'NE'
 	if orientacion > 67.5 and orientacion < 112.5:
 		return 'E'
-	if orientacion > 112.5 and orientacion < 157.5:
+	if orientacion >= 112.5 and orientacion <= 157.5:
 		return 'SE'
 	if orientacion > 157.5 and orientacion < 202.5:
 		return 'S'
-	if orientacion > 202.5 and orientacion < 245.5:
+	if orientacion >= 202.5 and orientacion <= 245.5:
 		return 'SO'
 	if orientacion > 245.5 and orientacion < 292.5:
 		return 'O'
-	if orientacion > 292.5 and orientacion < 337.5:
+	if orientacion >= 292.5 and orientacion <= 337.5:
 		return 'NO'
 
 print ''
 
 html =''
+listviento = []
+listorientacion = []
+listtemp_min = []
+listtemp_max = []
+provincias = ['Almeria','Cadiz','Cordoba','Granada','Huelva','Jaen','Malaga','Sevilla']
 
 for linea in f:
 	html += linea
 
-provincias = ['Almeria','Cadiz','Cordoba','Granada','Huelva','Jaen','Malaga','Sevilla']
 
 for provincia in provincias:
 	tiempo = requests.get('http://api.openweathermap.org/data/2.5/weather', params={'q':'%s,spain' % provincia})
 	jtemp = json.loads(tiempo.text)
-	#temp = jtemp['main']['temp']
-	speed = round(jtemp['wind']['speed']*1.609,1)
-	#orientacion = jtemp['wind']['deg']
-	#orientacion = direccion(orientacion)
-	temp_min = round(jtemp['main']['temp_min']-273,2)
-	temp_max = round(jtemp['main']['temp_max']-273,2)
-	mitemplate = Template(html)
-	mitemplate = mitemplate.render(provincia=provincia,temp_min=temp_min,temp_max=temp_max,viento=speed)
+	speed = int(jtemp['wind']['speed']*1.609)
+	listviento.append(speed)
+	orienta = jtemp['wind']['deg']
+	orientacion = direccion(orienta)
+	listorientacion.appendo(orientacion)
+	temp_min = int(jtemp['main']['temp_min']-273)
+	listtemp_min.appendo(temp_min)
+	temp_max = int(jtemp['main']['temp_max']-273)
+	listtemp_max.append(temp_max)
 
+
+mitemplate = Template(html)
+mitemplate = mitemplate.render(provincia=provincia,temp_min=listtemp_min,temp_max=listtemp_max,viento=listviento,direccion=listorientacion)
 web.write(mitemplate)
-
-
 
 webbrowser.open('web.html')
