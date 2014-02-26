595dd39ad42f46dd91b1f5f541572f8c97cb7e87
usuario <usuario@debian.gonzalonazareno.org>
2014-02-26 10:33:15 +0000

    provincias no provincia

--- tiempo.py
+++ tiempo.py
@@ -8,7 +8,7 @@ f = open('template.html','r')
 web = open('web.html','w')
 
 def direccion(orientacion):
-	if (orientacion > 337.5 and orientacion < 360) or (orientacion  > 0 and orientacion < 22.5):
+	if (orientacion > 337.5 and orientacion <= 360) or (orientacion  >= 0 and orientacion < 22.5):
 		return 'N'
 	if orientacion >= 22.5 and orientacion <= 67.5:
 		return 'NE'
@@ -32,7 +32,7 @@ listviento = []
 listorientacion = []
 listtemp_min = []
 listtemp_max = []
-provincias = ['Almeria','Cadiz','Cordoba','Granada','Huelva','Jaen','Malaga','Sevilla']
+provincias = ['Almeria','Cadiz','Cordoba','Huelva','Jaen','Malaga','Sevilla']
 
 for linea in f:
 	html += linea
@@ -41,9 +41,9 @@ for linea in f:
 for provincia in provincias:
 	tiempo = requests.get('http://api.openweathermap.org/data/2.5/weather', params={'q':'%s,spain' % provincia})
 	jtemp = json.loads(tiempo.text)
-	speed = int(jtemp['wind']['speed']*1.609)
+	speed = int(jtemp["wind"]['speed']*1.609)
 	listviento.append(speed)
-	orienta = jtemp['wind']['deg']
+	orienta = jtemp["wind"]['deg']
 	orientacion = direccion(orienta)
 	listorientacion.append(orientacion)
 	temp_min = int(jtemp['main']['temp_min']-273)
@@ -51,11 +51,8 @@ for provincia in provincias:
 	temp_max = int(jtemp['main']['temp_max']-273)
 	listtemp_max.append(temp_max)
 	
-
-
 mitemplate = Template(html)
-mitemplate = mitemplate.render(provincia=provincias,temp_min=listtemp_min,temp_max=listtemp_max,viento=listviento,direccion=listorientacion)
+mitemplate = mitemplate.render(provincias=provincias,temp_min=listtemp_min,temp_max=listtemp_max,viento=listviento,direccion=listorientacion)
 web.write(mitemplate)
-
 
 webbrowser.open('web.html')
